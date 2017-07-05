6cdc62d2e394c61f12ef8e237b25decff9b1117e
tan <tan@tans-Mac.local>
2017-07-05 13:43:02 -0700

    Learned URLSession with POST request

--- Member/Member/ViewController.swift
+++ Member/Member/ViewController.swift
@@ -15,12 +15,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.hideKeyboardWhenTappedAround()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
     @IBOutlet weak var textName: UITextField!
     
@@ -40,6 +43,13 @@ class ViewController: UIViewController {
         let memberCount = textMember.text
         
         //creating the post parameter by concatenating the keys and values from text field
+        if (teamName == "" || memberCount == "") {
+            
+            //self.labelMessage.text = "Please insert data"
+            let alert = UIAlertController(title: "Warning!", message: "Please insert data", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
         let postParameters = "name="+teamName!+"&member="+memberCount!;
         
         //adding the parameters to request body
@@ -50,7 +60,14 @@ class ViewController: UIViewController {
             (data, response, error) in
             
             if error != nil{
+                DispatchQueue.main.async
+                {
+                        let alert = UIAlertController(title: "Upload Didn't Work?", message: "Looks like the connection to the server didn't work.  Do you have Internet access?", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                }
                 print("error is \(String(describing: error))")
+                //self.labelMessage.text = "Error"
                 return;
             } //End if
                 //parsing the response
@@ -68,7 +85,14 @@ class ViewController: UIViewController {
                         msg = parseJSON["message"] as! String?
                         
                         //printing the response
-                        self.labelMessage.text = msg
+                        //self.labelMessage.text = msg
+                        DispatchQueue.main.async
+                            {
+                                let alert = UIAlertController(title: "Congratulation", message: msg, preferredStyle: .alert)
+                                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                                self.present(alert, animated: true, completion: nil)
+                        }
+
                         print(msg)
                         
                     } //end if
@@ -77,7 +101,24 @@ class ViewController: UIViewController {
                     print(error)
                 }//end catch
          }// End task
+        
             //executing the task
             task.resume()
+            //clear the textview
+            textName.text = ""
+            textMember.text = ""
         }
+    }
     }//end btn
+//Hide the keyboard
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
